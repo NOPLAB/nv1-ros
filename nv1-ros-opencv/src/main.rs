7877bb4382e36daa4484bfba259ab365afcc93a1
nop <noplab90@gmail.com>
2025-02-14 15:27:45 +0900

    process front and rear camera

--- nv1-ros-opencv/src/main.rs
+++ nv1-ros-opencv/src/main.rs
@@ -4,7 +4,7 @@ use opencv::{
     core::{GpuMat, Point, Scalar, Size, Stream, VecN, Vector},
     cudaarithm, cudaimgproc, highgui, imgproc,
     prelude::*,
-    videoio,
+    videoio::{self, VideoCapture},
 };
 use tokio::{select, task};
 
@@ -39,6 +39,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let window_tuner = "opencv tuner";
             highgui::named_window(window_tuner, 0)?;
 
+            let window_front = "opencv front";
+            highgui::named_window(&window_front, 0)?;
+
+            let window_rear = "opencv rear";
+            highgui::named_window(&window_rear, 0)?;
+
             let mut h_min = 0;
             highgui::create_trackbar("H_min", &window_tuner, Some(&mut h_min), 255, None)?;
             let mut h_max = 255;
@@ -51,148 +57,71 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             highgui::create_trackbar("V_min", &window_tuner, Some(&mut v_min), 255, None)?;
             let mut v_max = 255;
             highgui::create_trackbar("V_max", &window_tuner, Some(&mut v_max), 255, None)?;
+            let mut area_threshold = 2000;
+            highgui::create_trackbar(
+                "Area Threthold",
+                &window_tuner,
+                Some(&mut area_threshold),
+                10000,
+                None,
+            )?;
 
-            let mut cap_front = videoio::VideoCapture::from_file(
+            let cap_front = videoio::VideoCapture::from_file(
                 &gstreamer_pipeline(1, 1280, 720, 1280, 720, 60, 2),
                 videoio::CAP_GSTREAMER,
             )?;
 
-            let mut cap_back = videoio::VideoCapture::from_file(
+            let cap_rear = videoio::VideoCapture::from_file(
                 &gstreamer_pipeline(0, 1280, 720, 1280, 720, 60, 2),
                 videoio::CAP_GSTREAMER,
             )?;
 
-            let mut stream = Stream::default()?;
-
-            let mut gpu_frame_front = GpuMat::new_def()?;
-            let mut gpu_frame_front_yuv = GpuMat::new_def()?;
-            let mut gpu_frame_front_yuv_split = Vector::new();
-            let mut gpu_frame_front_rgb_clahed = GpuMat::new_def()?;
-            let mut gpu_frame_front_hsv_clahed = GpuMat::new_def()?;
-            let mut gpu_mask: GpuMat = GpuMat::new_def()?;
-            let mut gpu_tmp: GpuMat = GpuMat::new_def()?;
-            let mut dst = Mat::default();
-            let mut mask = Mat::default();
-            let mut labels = Mat::default();
-            let mut stats = Mat::default();
-            let mut centroids = Mat::default();
+            let mut processor_front = OpenCVProcessor::new(cap_front)?;
+            let mut processor_rear = OpenCVProcessor::new(cap_rear)?;
 
             loop {
-                let mut frame_front = Mat::default();
-                if cap_front.read(&mut frame_front)? {
-                    // let mut frame_front_bluer = Mat::default();
-                    // imgproc::gaussian_blur_def(
-                    //     &frame_front,
-                    //     &mut frame_front_bluer,
-                    //     Size::new(5, 5),
-                    //     0.0,
-                    // )?;
+                let processor_front_result = processor_front.process(
+                    h_min as f64,
+                    s_min as f64,
+                    v_min as f64,
+                    h_max as f64,
+                    s_max as f64,
+                    v_max as f64,
+                    area_threshold,
+                )?;
+                let processor_rear_result = processor_rear.process(
+                    h_min as f64,
+                    s_min as f64,
+                    v_min as f64,
+                    h_max as f64,
+                    s_max as f64,
+                    v_max as f64,
+                    area_threshold,
+                )?;
 
-                    gpu_frame_front.upload(&frame_front)?;
-
-                    cudaimgproc::cvt_color(
-                        &gpu_frame_front,
-                        &mut gpu_frame_front_yuv,
-                        imgproc::COLOR_RGB2YUV,
+                for rect in processor_front_result {
+                    imgproc::rectangle(
+                        &mut processor_front.frame_result,
+                        rect,
+                        Scalar::new(0.0, 255.0, 0.0, 0.0),
+                        2,
+                        8,
                         0,
-                        &mut stream,
                     )?;
-
-                    let mut clahe = cudaimgproc::create_clahe(2.0, Size::new(8, 8))?;
-
-                    cudaarithm::split_1(
-                        &gpu_frame_front_yuv,
-                        &mut gpu_frame_front_yuv_split,
-                        &mut stream,
-                    )?;
-
-                    let mut gpu_frame_front_channel_clahed = GpuMat::new_def()?;
-                    cudaimgproc::CUDA_CLAHETrait::apply(
-                        &mut clahe,
-                        &gpu_frame_front_yuv_split.get(2)?,
-                        &mut gpu_frame_front_channel_clahed,
-                        &mut stream,
-                    )?;
-
-                    gpu_frame_front_yuv_split.set(2, gpu_frame_front_channel_clahed)?;
-                    cudaarithm::merge_1(
-                        &gpu_frame_front_yuv_split,
-                        &mut gpu_frame_front_yuv,
-                        &mut stream,
-                    )?;
-
-                    cudaimgproc::cvt_color(
-                        &gpu_frame_front_yuv,
-                        &mut gpu_frame_front_rgb_clahed,
-                        imgproc::COLOR_YUV2RGB,
-                        0,
-                        &mut stream,
-                    )?;
-
-                    cudaimgproc::cvt_color(
-                        &gpu_frame_front_rgb_clahed,
-                        &mut gpu_frame_front_hsv_clahed,
-                        imgproc::COLOR_RGB2HSV,
-                        0,
-                        &mut stream,
-                    )?;
-
-                    cudaarithm::in_range(
-                        &gpu_frame_front_hsv_clahed,
-                        VecN::new(h_min as f64, s_min as f64, v_min as f64, 0.0),
-                        VecN::new(h_max as f64, s_max as f64, v_max as f64, 0.0),
-                        &mut gpu_mask,
-                        &mut stream,
-                    )?;
-                    cudaarithm::bitwise_not(
-                        &gpu_frame_front,
-                        &mut gpu_tmp,
-                        &gpu_mask,
-                        &mut stream,
-                    )?;
-
-                    let mut gpu_result = GpuMat::new_def()?;
-                    cudaarithm::bitwise_not(&gpu_tmp, &mut gpu_result, &gpu_mask, &mut stream)?;
-
-                    stream.wait_for_completion()?;
-
-                    gpu_result.download(&mut dst)?;
-
-                    gpu_mask.download(&mut mask)?;
-
-                    imgproc::connected_components_with_stats_def(
-                        &mask,
-                        &mut labels,
-                        &mut stats,
-                        &mut centroids,
-                    )?;
-
-                    for i in 1..stats.rows() {
-                        let area = stats.at_pt::<i32>(Point::new(imgproc::CC_STAT_AREA, i))?;
-
-                        if *area > 2000 {
-                            let left = stats.at_pt::<i32>(Point::new(imgproc::CC_STAT_LEFT, i))?;
-                            let top = stats.at_pt::<i32>(Point::new(imgproc::CC_STAT_TOP, i))?;
-                            let width =
-                                stats.at_pt::<i32>(Point::new(imgproc::CC_STAT_WIDTH, i))?;
-                            let height =
-                                stats.at_pt::<i32>(Point::new(imgproc::CC_STAT_HEIGHT, i))?;
-                            let rect = opencv::core::Rect::new(*left, *top, *width, *height);
-
-                            imgproc::rectangle(
-                                &mut dst,
-                                rect,
-                                Scalar::new(0.0, 255.0, 0.0, 0.0),
-                                2,
-                                8,
-                                0,
-                            )?;
-                        }
-                    }
-
-                    // highgui::imshow(window_camera, &dst)?;
-                    highgui::imshow(&window_tuner, &dst)?;
                 }
+                for rect in processor_rear_result {
+                    imgproc::rectangle(
+                        &mut processor_rear.frame_result,
+                        rect,
+                        Scalar::new(0.0, 255.0, 0.0, 0.0),
+                        2,
+                        8,
+                        0,
+                    )?;
+                }
+
+                highgui::imshow(&window_front, &processor_front.frame_result)?;
+                highgui::imshow(&window_rear, &processor_rear.frame_result)?;
 
                 let key = opencv::highgui::wait_key(1)?;
                 if key == 27 {
@@ -216,4 +145,167 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .unwrap();
 
     Ok(())
+}
+
+pub struct OpenCVProcessor {
+    capture: VideoCapture,
+    gpu_frame: GpuMat,
+    gpu_frame_yuv: GpuMat,
+    gpu_frame_yuv_split: Vector<GpuMat>,
+    gpu_frame_rgb_clahed: GpuMat,
+    gpu_frame_hsv_clahed: GpuMat,
+    gpu_frame_masked: GpuMat,
+    gpu_frame_tmp: GpuMat,
+    gpu_frame_result: GpuMat,
+    pub frame_result: Mat,
+    pub frame_masked: Mat,
+    pub labels: Mat,
+    pub stats: Mat,
+    pub centroids: Mat,
+}
+
+impl OpenCVProcessor {
+    pub fn new(capture: VideoCapture) -> Result<Self, opencv::Error> {
+        Ok(OpenCVProcessor {
+            capture,
+            gpu_frame: GpuMat::new_def()?,
+            gpu_frame_yuv: GpuMat::new_def()?,
+            gpu_frame_yuv_split: Vector::new(),
+            gpu_frame_rgb_clahed: GpuMat::new_def()?,
+            gpu_frame_hsv_clahed: GpuMat::new_def()?,
+            gpu_frame_masked: GpuMat::new_def()?,
+            gpu_frame_tmp: GpuMat::new_def()?,
+            gpu_frame_result: GpuMat::new_def()?,
+            frame_result: Mat::default(),
+            frame_masked: Mat::default(),
+            labels: Mat::default(),
+            stats: Mat::default(),
+            centroids: Mat::default(),
+        })
+    }
+
+    pub fn process(
+        &mut self,
+        h_min: f64,
+        s_min: f64,
+        v_min: f64,
+        h_max: f64,
+        s_max: f64,
+        v_max: f64,
+        area_threshold: i32,
+    ) -> Result<Vec<opencv::core::Rect>, opencv::Error> {
+        let mut frame = Mat::default();
+        self.capture.read(&mut frame)?;
+        self.gpu_frame.upload(&frame)?;
+
+        let mut stream = Stream::default()?;
+
+        cudaimgproc::cvt_color(
+            &self.gpu_frame,
+            &mut self.gpu_frame_yuv,
+            imgproc::COLOR_RGB2YUV,
+            0,
+            &mut stream,
+        )?;
+
+        let mut clahe = cudaimgproc::create_clahe(2.0, Size::new(8, 8))?;
+
+        cudaarithm::split_1(
+            &self.gpu_frame_yuv,
+            &mut self.gpu_frame_yuv_split,
+            &mut stream,
+        )?;
+
+        let mut gpu_frame_front_channel_clahed = GpuMat::new_def()?;
+        cudaimgproc::CUDA_CLAHETrait::apply(
+            &mut clahe,
+            &self.gpu_frame_yuv_split.get(2)?,
+            &mut gpu_frame_front_channel_clahed,
+            &mut stream,
+        )?;
+
+        self.gpu_frame_yuv_split
+            .set(2, gpu_frame_front_channel_clahed)?;
+        cudaarithm::merge_1(
+            &self.gpu_frame_yuv_split,
+            &mut self.gpu_frame_yuv,
+            &mut stream,
+        )?;
+
+        cudaimgproc::cvt_color(
+            &self.gpu_frame_yuv,
+            &mut self.gpu_frame_rgb_clahed,
+            imgproc::COLOR_YUV2RGB,
+            0,
+            &mut stream,
+        )?;
+
+        cudaimgproc::cvt_color(
+            &self.gpu_frame_rgb_clahed,
+            &mut self.gpu_frame_hsv_clahed,
+            imgproc::COLOR_RGB2HSV,
+            0,
+            &mut stream,
+        )?;
+
+        cudaarithm::in_range(
+            &self.gpu_frame_hsv_clahed,
+            VecN::new(h_min, s_min, v_min, 0.0),
+            VecN::new(h_max, s_max, v_max, 0.0),
+            &mut self.gpu_frame_masked,
+            &mut stream,
+        )?;
+        cudaarithm::bitwise_not(
+            &self.gpu_frame,
+            &mut self.gpu_frame_tmp,
+            &self.gpu_frame_masked,
+            &mut stream,
+        )?;
+
+        cudaarithm::bitwise_not(
+            &self.gpu_frame_tmp,
+            &mut self.gpu_frame_result,
+            &self.gpu_frame_masked,
+            &mut stream,
+        )?;
+
+        stream.wait_for_completion()?;
+
+        self.gpu_frame_result.download(&mut self.frame_result)?;
+
+        self.gpu_frame_masked.download(&mut self.frame_masked)?;
+
+        imgproc::connected_components_with_stats_def(
+            &self.frame_masked,
+            &mut self.labels,
+            &mut self.stats,
+            &mut self.centroids,
+        )?;
+
+        let mut detected_rects = Vec::new();
+        for i in 1..self.stats.rows() {
+            let area = self
+                .stats
+                .at_pt::<i32>(Point::new(imgproc::CC_STAT_AREA, i))?;
+
+            if *area > area_threshold {
+                let left = self
+                    .stats
+                    .at_pt::<i32>(Point::new(imgproc::CC_STAT_LEFT, i))?;
+                let top = self
+                    .stats
+                    .at_pt::<i32>(Point::new(imgproc::CC_STAT_TOP, i))?;
+                let width = self
+                    .stats
+                    .at_pt::<i32>(Point::new(imgproc::CC_STAT_WIDTH, i))?;
+                let height = self
+                    .stats
+                    .at_pt::<i32>(Point::new(imgproc::CC_STAT_HEIGHT, i))?;
+                let rect = opencv::core::Rect::new(*left, *top, *width, *height);
+                detected_rects.push(rect);
+            }
+        }
+
+        Ok(detected_rects)
+    }
 }
